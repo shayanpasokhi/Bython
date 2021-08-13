8fcc2146ce02a31a415b544935b21f0befec9fe3
shayan <shayanrazavi33@yahoo.com>
2021-08-14 00:41:45 +0430

    complete Lexer, Token, AST, Parser, regex & Interpreter class

--- Bython.php
+++ Bython.php
@@ -13,6 +13,7 @@ define('VOROODI', 'VOROODI');
 define('KHOOROOJI', 'KHOOROOJI');
 define('AGAR', 'AGAR');
 define('EQUAL', 'EQUAL');
+define('SEMI', 'SEMI');
 define('EOF', 'EOF');
 
 class Token{
@@ -99,6 +100,12 @@ class Lexer{
                 continue;
             }
 
+            if($this->currentChar == ';'){
+                $this->advance();
+
+                return new Token(SEMI, ';');
+            }
+
             if(ctype_digit($this->currentChar)){
                 return new Token(NUMBER, $this->number());
             }
@@ -161,3 +168,446 @@ class Lexer{
         return new Token(EOF, null);
     }
 }
+
+class AST{
+
+}
+
+class Compound extends AST{
+    public $children;
+
+    public function __construct(){
+        $this->children = [];
+    }
+}
+
+class Assign extends AST{
+    public $left;
+    public $op;
+    public $right;
+
+    public function __construct($left, $op, $right){
+        $this->left = $left;
+        $this->op = $op;
+        $this->right = $right;
+    }
+}
+
+class _Var extends AST{
+    public $token;
+
+    public function __construct($token){
+        $this->token = $token;
+    }
+}
+
+class Num extends AST{
+    public $token;
+
+    public function __construct($token){
+        $this->token = $token;
+    }
+}
+
+class BinOp extends AST{
+    public $left;
+    public $op;
+    public $right;
+
+    public function __construct($left, $op, $right){
+        $this->left = $left;
+        $this->op = $op;
+        $this->right = $right;
+    }
+}
+
+class Input extends AST{
+    public $token;
+
+    public function __construct($token){
+        $this->token = $token;
+    }
+}
+
+class _Print extends AST{
+    public $token;
+    public $list;
+
+    public function __construct($token){
+        $this->token = $token;
+        $this->list = [];
+    }
+}
+
+class _If extends AST{
+    public $token;
+    public $condition;
+    public $op;
+    public $assignment;
+
+    public function __construct($token, $condition, $op, $assignment){
+        $this->token = $token;
+        $this->condition = $condition;
+        $this->op = $op;
+        $this->assignment = $assignment;
+    }
+}
+
+class Condition extends AST{
+    public $left;
+    public $op;
+    public $right;
+
+    public function __construct($left, $op, $right){
+        $this->left = $left;
+        $this->op = $op;
+        $this->right = $right;
+    }
+}
+
+class NoOp extends AST{
+
+}
+
+class Parser{
+    public $lexer;
+    public $currentToken;
+
+    public function __construct($lexer){
+        $this->lexer = $lexer;
+        $this->currentToken = $this->lexer->getNextToken();
+    }
+
+    public function error($message = 'Invalid syntax'){
+        echo $message;
+        exit();
+    }
+
+    public function eat($tokenType){
+        if($this->currentToken->type == $tokenType){
+            $this->currentToken = $this->lexer->getNextToken();
+        }else{
+            $this->error();
+        }
+    }
+
+    public function program(){
+        return $this->compoundStatement();
+    }
+
+    public function compoundStatement(){
+        $node = $this->statementList();
+        $result = new Compound();
+
+        foreach($node as $item){
+            $result->children[] = $item;
+        }
+
+        return $result;
+    }
+
+    public function statementList(){
+        $node = $this->statement();
+        $result = [$node];
+
+        while($this->currentToken->type == SEMI){
+            $this->eat(SEMI);
+            $result[] = $this->statement();
+        }
+
+        return $result;
+    }
+
+    public function statement(){
+        if($this->currentToken->type == ID){
+            $node = $this->assignment();
+        }elseif($this->currentToken->type == VOROODI){
+            $node = $this->input();
+        }elseif($this->currentToken->type == KHOOROOJI){
+            $node = $this->_print();
+        }elseif($this->currentToken->type == AGAR){
+            $node = $this->_if();
+        }elseif($this->currentToken->type == SEMI){
+            $node = $this->empty();
+        }else{
+            $this->error();
+        }
+
+        return $node;
+    }
+
+    public function empty(){
+        return new NoOp();
+    }
+
+    public function assignment(){
+        $left = $this->variable();
+        $op = $this->currentToken;
+        $this->eat(ASSIGN);
+
+        return new Assign($left, $op, $this->expr());
+    }
+
+    public function variable(){
+        $token = $this->currentToken;
+        $this->eat(ID);
+
+        return new _Var($token);
+    }
+
+    public function expr(){
+        if($this->currentToken->type == VOROODI){
+            return $this->input();
+        }else{
+            $left = $this->arg();
+
+            if(in_array($this->currentToken->type, [PLUS, MINUS])){
+                $op = $this->currentToken;
+
+                if($this->currentToken->type == PLUS){
+                    $this->eat(PLUS);
+                }else{
+                    $this->eat(MINUS);
+                }
+
+                return new BinOp($left, $op, $this->arg());
+            }
+
+            return $left;
+        }
+    }
+
+    public function input(){
+        $token = $this->currentToken;
+        $this->eat(VOROODI);
+        $this->eat(LPAREN);
+        $this->eat(RPAREN);
+
+        return new Input($token);
+    }
+
+    public function arg(){
+        if($this->currentToken->type == NUMBER){
+            $token = $this->currentToken;
+            $this->eat(NUMBER);
+
+            return new Num($token);
+        }else{
+            return $this->variable();
+        }
+    }
+
+    public function _print(){
+        $token = $this->currentToken;
+        $this->eat(KHOOROOJI);
+        $this->eat(LPAREN);
+        $_print = new _Print($token);
+        $_print->list = $this->list();
+        $this->eat(RPAREN);
+        
+        return $_print;
+    }
+
+    public function list(){
+        $node = $this->arg();
+        $result = [$node];
+
+        while($this->currentToken->type == COMMA){
+            $this->eat(COMMA);
+            $result[] = $this->arg();
+        }
+
+        return $result;
+    }
+
+    public function _if(){
+        $token = $this->currentToken;
+        $this->eat(AGAR);
+        $condition = $this->condition();
+        $op = $this->currentToken;
+        $this->eat(COLON);
+
+        return new _If($token, $condition, $op, $this->assignment());
+    }
+
+    public function condition(){
+        $left = $this->arg();
+        $op = $this->currentToken;
+        $this->eat(EQUAL);
+
+        return new Condition($left, $op, $this->arg());
+    }
+
+    public function parse(){
+        $node = $this->program();
+
+        if($this->currentToken->type != EOF){
+            $this->error();
+        }
+
+        return $node;
+    }
+}
+
+class NodeVisitor{
+    public function visit($node){
+        $methodName = 'visit' . get_class($node);
+
+        if(method_exists(get_class($this), $methodName)){
+            return $this->{$methodName}($node);
+        }else{
+            $this->genericVisit($node);
+        }
+    }
+
+    public function genericVisit($node){
+        echo 'No visit' . get_class($node) . ' method';
+        exit();
+    }
+}
+
+class Interpreter extends NodeVisitor{
+    public $GLOBAL_SCOPE;
+    public $parser;
+    public $input;
+
+    public function __construct($parser, $input){
+        $this->parser = $parser;
+        $this->GLOBAL_SCOPE = [];
+        $this->input = $input;
+    }
+
+    public function visitCompound($node){
+        foreach($node->children as $item){
+            $this->visit($item);
+        }
+    }
+
+    public function visitAssign($node){
+        $varName = $node->left->token->value;
+        $this->GLOBAL_SCOPE[$varName] = $this->visit($node->right);
+    }
+
+    public function visit_Var($node){
+        $varName = $node->token->value;
+        
+        if(isset($this->GLOBAL_SCOPE[$varName])){
+            return $this->GLOBAL_SCOPE[$varName];
+        }else{
+            exit();
+        }
+    }
+
+    public function visitNum($node){
+        return $node->token->value;
+    }
+
+    public function visitBinOp($node){
+        if($node->op->type == PLUS){
+            return $this->visit($node->left) + $this->visit($node->right);
+        }elseif($node->op->type == MINUS){
+            return $this->visit($node->left) - $this->visit($node->right);
+        }
+    }
+
+    public function visitInput($node){
+        return array_shift($this->input);
+    }
+
+    public function visit_Print($node){
+        $result = '';
+
+        foreach($node->list as $item){
+            $result .= $this->visit($item) . ' ';
+        }
+
+        echo rtrim($result, ' ') . PHP_EOL;
+    }
+
+    public function visit_If($node){
+        if($this->visit($node->condition)){
+            $this->visit($node->assignment);
+        }
+    }
+
+    public function visitCondition($node){
+        if($this->visit($node->left) == $this->visit($node->right)){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public function visitNoOp(){
+        
+    }
+
+    public function interpret(){
+        $tree = $this->parser->parse();
+
+        if(is_null($tree)){
+            return '';
+        }
+
+        return $this->visit($tree);
+    }
+}
+
+function completePattern($pattern){
+    return '/' . $pattern . '/';
+}
+
+function absPattern($pattern){
+    return '^' . $pattern . '$';
+}
+
+function preAnalysis($text, $pattern){
+    foreach($pattern as $item){
+        if(preg_match(completePattern($item), $text)){
+            return true;
+        }
+    }
+
+    return false;
+}
+
+$text = [];
+$input = [];
+$inputCount = 0;
+
+$assignPattern = '\ *\=\ *';
+$equalPattern = '\ *\=\=\ *';
+$numberPattern = '\ *\d+\ *';
+$variablePattern = '\ *[a-zA-Z][a-zA-Z0-9]{0,9}\ *';
+$argPattern = '\ *(' . $numberPattern . '|' . $variablePattern . ')\ *';
+$inputPattern = '\ *voroodi\ *\(\ *\)\ *';
+$listPattern = '\ *' . $argPattern . '\ *(\,\ *' . $argPattern . ')*\ *';
+$printPattern = '\ *khoorooji\ *\(\ *' . $listPattern . '\ *\)\ *';
+$conditionPattern = '\ *' . $argPattern . '\ *' . $equalPattern . '\ *' . $argPattern . '\ *';
+$exprPattern = '\ *(' . $numberPattern . '|' . $inputPattern . '|' . $argPattern . '\ *(\+|\-)\ *' . $argPattern . ')\ *';
+$assignmentPattern = '\ *' . $variablePattern . '\ *' . $assignPattern . '\ *' . $exprPattern . '\ *';
+$ifPattern = '\ *agar\ *' . $conditionPattern . '\ *\:\ *' . $assignmentPattern . '\ *';
+
+$pattern = array_map(fn($value) => absPattern($value), [$inputPattern, $printPattern, $ifPattern, $assignmentPattern]);
+
+while(($line = readline()) != '-----'){
+    if(!$line || !preAnalysis($line, $pattern)){
+        continue;
+    }
+
+    if(preg_match(completePattern($inputPattern), $line)){
+        ++$inputCount;
+    }
+
+    $text[] = $line;
+}
+
+for($i = 0; $i < $inputCount; $i++){
+    $input[] = readline();
+}
+
+$lexer = new Lexer(implode(';', $text), $RESERVED_KEYWORD);
+$parser = new Parser($lexer);
+$interpreter = new Interpreter($parser, $input);
+$interpreter->interpret();
+echo count($interpreter->GLOBAL_SCOPE);
--- README.md
+++ README.md
@@ -1,1 +1,101 @@
+
 # Bython
+
+*php 7.4.0 or higher is required*
+
+---
+
+grammar:
+
+    program: compoundStatement
+    compoundStatement: statementList
+    statementList: statement | statementList
+    statement: assignment | input | print | if | empty
+    if: AGAR condition COLON assignment
+    condition: arg EQUAL arg
+    assignment: variable ASSIGN expr
+    expr: NUMBER | input | arg (PLUS|MINUS) arg
+    input: VOROODI LPAREN RPAREN
+    print: KHOOROOJI LPAREN list RPAREN
+    list: arg (COMMA arg)*
+    arg: NUMBER | variable
+    variable: ID
+    empty:
+
+---
+
+**Input**
+
+The input consists of two parts, in the first part of the program commands are in several lines and then in a string line `-----` then the program inputs are in one line each.
+
+ - All numbers given in the instructions must be natural and positive numbers, but the value of the variable may be negative when executing commands.
+
+---
+
+**Output**
+
+  
+In output, for each `print` command, its output is printed on one line. Then in one line, the number of variables that have been set at least once is output
+
+ ---
+ 
+**Example**
+
+Sample input:
+
+```undefined
+A = 3
+B = voroodi()
+C = A + B
+khoorooji(10)
+khoorooji(C, 2)
+-----
+3
+```
+
+Sample output:
+
+```undefined
+10
+6 2
+3
+```
+
+---
+
+Sample input:
+
+```undefined
+A = voroodi()
+agar A == 2 : A = A + 2
+khoorooji(A)
+-----
+2
+```
+
+Sample output:
+
+```undefined
+4
+1
+```
+
+---
+
+Sample input:
+
+```undefined
+agar 2 == 2 : A = 3
+agar 3 == 4 : B = 1
+khoorooji(A)
+-----
+```
+
+Sample output:
+
+```undefined
+3
+1
+```
+
+---
